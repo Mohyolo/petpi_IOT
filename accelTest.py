40a79f8e4cdd625d8645b3b957a889bc3c529f17
Mohamed-Amiin Mohamed <D00245319@student.dkit.ie>
2024-11-13 20:53:58 +0000

    added GPS test code
    Changed config of raspberry pi  such as stopping and disabling one of the Serial process so to get the data from the sensors using a python script

---
+++ accelTest.py
@@ -0,0 +1,15 @@
+#code is from the below tutorial
+#https://www.youtube.com/watch?v=QH1umP-duik
+
+import time
+import board
+import busio
+import adafruit_adxl34x
+
+i2c = busio.I2C(board.SCL,board.SDA)
+accelerometer = adafruit_adxl34x.ADXL345(i2c)
+
+
+while True:
+    print("%f %f %f"%accelerometer.acceleration)
+    time.sleep(1)
