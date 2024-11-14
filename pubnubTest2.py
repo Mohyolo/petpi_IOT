7f3a244f4ef357345e92765c72bd03a0bf7cadcc
Mohamed-Amiin Mohamed <D00245319@student.dkit.ie>
2024-11-14 20:07:33 +0000

    Fixed code for pubnubTest2
    there were some code i misspelled.

--- pubnubTest2.py
+++ pubnubTest2.py
@@ -3,7 +3,7 @@ from pubnub import Pubnub
 
 pubnub = Pubnub(publish_key='pub-c-6478efda-63f9-4d8c-84ba-df3504a89d76', subscribe_key='sub-c-f8959022-48fd-4250-99bc-3a437eb6fd64')
 
-channel = 'pi'
+channel = 'GPSpetpi'
 
 data = {
   'username': 'Petpi',
