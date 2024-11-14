5727aa53d90b4ceba87b1ecaaa4ed9ca67d0c35b
Mohamed-Amiin Mohamed <D00245319@student.dkit.ie>
2024-11-14 18:42:23 +0000

    Re-Made pubnubTest
    PubNub not working at all. thus remade it to work in a simple subscribe and publish a simple message. This as done in order to test the connection to pubnub.

--- pubnubTest.py
+++ pubnubTest.py
@@ -1,88 +1,106 @@
-#code from both
+#code references
 #https://www.electronicwings.com/raspberry-pi/gps-module-interfacing-with-raspberry-pi
-#and
 #https://www.pubnub.com/docs/sdks/python
+#https://drive.google.com/drive/folders/1K0ZHI6oBDX4PYjixL9p1f6kPq-q0u7gS from https://www.youtube.com/watch?v=meT2WB369mc
 
-import serial               #import serial pacakge
-import time
-import sys
+###############################################################################
+# Board: Raspberry Pi 3 (model B)
+# Developed by: Akshay P Daga       Email: apdaga@gmail.com
+# Programming Language: Python 
+# This Program is just to demonstrate the Publish-Subscribe IoT Architecture
+#
+# Description:
+# PUBNUB is the Broker/Server based on Public-Subscribe Architecture.
+#
+# STEPS:
+# 1. Open a free account on https://www.pubnub.com/ 
+#    a. Click on "Create New App" > Enter <Name for the App> > Click on "Create"
+#    b. Click on the Newly created app
+#    c. You can see that, Public Key and Subscribe Key
+#    
+# 2. Open Console for Debugging purpose
+#    a. Go to  https://www.pubnub.com/docs/console
+#    b. Copy and paste Public Key and Subscribe Key from step 1.c. to console window
+#    c. Change the Name of the Channel to "Raspberry"
+#    d. Click on the "SUBSCRIBE" > Conncection status will change to "CONNECTED" 
+#    
+#    e. Scroll down to (white) "message" window and write following in json format:
+#    	  {"text":"Hi Guys"}
+#    	  Click on "Send" button
+# 	  
+# 	  you will see the same message in black coloured message console.
+# 	  
+# 	  That means, It is working fine 
+#    
+# 3. Install punnub python library on your RPi3:
+#    a. Open terminal on RPi3
+#    b. type following command:
+#       sudo pip install pubnub
+# 	  
+# 4. Place python script "ps_01.py" or "ps_02.py" on RPi3
+#    Change Following 3 thing in these script:
+#    a. pubnub publish_key  (from step 1.c)
+#    b. pubnub subscibe_key (from step 1.c)
+#    c. pubnub channel_name (from step 2.c)
+#    
+# 5. Run these Scripts on RPi3 using "Python2" IDE or in Terminal
+#    
+# 6. In this script, we are published and subcribed to the same borker.channel
+#    Here, we are connecting/subscribing to the Borker.channel
+#    then publishing the data once to the server/Broker.channel
+#    and then reading the received msgs in infinite loop.
+#    
+# 7. We can Debug this program using pubnub console (https://www.pubnub.com/docs/console),
+#    a. Run this script on RPi3 (as shown in step 5)
+#    b. set publish_key, subscribe_key and channel_name in pubnub console same as the script,
+#    c. and publish the data from pubnub console (as shown in step 2.e) 
+#    
+#    you can observer same msg received on RPi3 Terminal (where script is running)
+#
+######################################################################################    
 
+# importing pubnub libraries
+from pubnub.pubnub import PubNub, SubscribeListener, SubscribeCallback, PNStatusCategory
 from pubnub.pnconfiguration import PNConfiguration
-from pubnub.pubnub import PubNub, SubscribeListener
+from pubnub.exceptions import PubNubException
+import pubnub
 
-class Listener(SubscribeListener):
+
+pnconf = PNConfiguration()                                              # create pubnub_configuration_object
+ 
+pnconf.publish_key = 'pub-c-6478efda-63f9-4d8c-84ba-df3504a89d76'       # set pubnub publish_key
+pnconf.subscribe_key = 'sub-c-f8959022-48fd-4250-99bc-3a437eb6fd64'     # set pubnub subscibe_key
+ 
+pubnub = PubNub(pnconf)                     # create pubnub_object using pubnub_configuration_object
+
+channel='GPS-Petpi'                         # provide pubnub channel_name
+
+data = {                                    # data to be published
+    'username': 'Akshay: ps_01',
+    'message' : 'This is my 1st msg... '
+}
+
+class MyListener(SubscribeCallback):        # Not need for working of this program
     def status(self, pubnub, status):
-        print(f'Status: \n{status.category.name}')
+        if status.category == PNStatusCategory.PNConnectedCategory:
+            pubnub.publish().channel(channel).message({'fieldA': 'awesome', 'fieldB': 10}).sync()
+ 
+    def message(self, pubnub, message):
+        print(message)
+ 
+    def presence(self, pubnub, presence):
+        pass
 
-def GPS_Info():
-    global NMEA_buff
-    global lat_in_degrees
-    global long_in_degrees
-    nmea_time = []
-    nmea_latitude = []
-    nmea_longitude = []
-    nmea_time = NMEA_buff[0]                    #extract time from GPGGA string
-    nmea_latitude = NMEA_buff[1]                #extract latitude from GPGGA string
-    nmea_longitude = NMEA_buff[3]               #extract longitude from GPGGA string
-    
-    print("NMEA Time: ", nmea_time,'\n')
-    print ("NMEA Latitude:", nmea_latitude,"NMEA Longitude:", nmea_longitude,'\n')
-    
-    lat = float(nmea_latitude)                  #convert string into float for calculation
-    longi = float(nmea_longitude)               #convertr string into float for calculation
-    
-    lat_in_degrees = convert_to_degrees(lat)    #get latitude in degree decimal format
-    long_in_degrees = convert_to_degrees(longi) #get longitude in degree decimal format
-    
-#convert raw NMEA string into degree decimal format   
-def convert_to_degrees(raw_value):
-    decimal_value = raw_value/100.00
-    degrees = int(decimal_value)
-    mm_mmmm = (decimal_value - int(decimal_value))/0.6
-    position = degrees + mm_mmmm
-    position = "%.4f" %(position)
-    return position
-    
+my_listener = SubscribeListener()                   # create listner_object to read the msg from the Broker/Server
+pubnub.add_listener(my_listener)                    # add listner_object to pubnub_object to subscribe it
+pubnub.subscribe().channels(channel).execute()      # subscribe the channel (Runs in background)
 
-# here we create configuration for our pubnub instance
-config = PNConfiguration()
-config.subscribe_key = 'demo'
-config.publish_key = 'demo'
-config.user_id = 'example'
-config.enable_subscribe = True
-config.daemon = True
 
-pubnub = PubNub(config)
-pubnub.add_listener(Listener())
+my_listener.wait_for_connect()                      # wait for the listner_obj to connect to the Broker.Channel 
+print('connected')                                  # print confirmation msg
 
-subscription = pubnub.channel('example').subscription()
-subscription.on_message = lambda message: print(f'Message from {message.publisher}: {message.message}')
-subscription.subscribe()
+pubnub.publish().channel(channel).message(data).sync()      # publish the data to the mentioned channel
 
-gpgga_info = "$GPGGA,"
-ser = serial.Serial ("/dev/ttyAMA0")              #Open port with baud rate
-GPGGA_buffer = 0
-NMEA_buff = 0
-lat_in_degrees = 0
-long_in_degrees = 0
-
-try:
-    while True:
-        received_data = (str)(ser.readline())                   #read NMEA string received
-        GPGGA_data_available = received_data.find(gpgga_info)   #check for NMEA GPGGA string                 
-        if (GPGGA_data_available>0):
-            GPGGA_buffer = received_data.split("$GPGGA,",1)[1]  #store data coming after "$GPGGA," string 
-            NMEA_buff = (GPGGA_buffer.split(','))               #store comma separated data in buffer
-            GPS_Info()                                          #get time, latitude, longitude
-
-            messageOut = f"lat in degrees: {lat_in_degrees}, long in degree: {long_in_degrees}"
-
-            publish_result = pubnub.publish().channel("example").message(messageOut).sync()
-
-            print("lat in degrees:", lat_in_degrees," long in degree: ", long_in_degrees, '\n')
-            map_link = 'http://maps.google.com/?q=' + lat_in_degrees + ',' + long_in_degrees    #create link to plot location on Google map
-            print("<<<<<<<<press ctrl+c to plot location on google maps>>>>>>\n")               #press ctrl+c to plot on map and exit 
-            print("------------------------------------------------------------\n")
-                        
-except KeyboardInterrupt:
-    sys.exit(0)
+while True:                                                 # Infinite loop
+    result = my_listener.wait_for_message_on(channel)       # Read the new msg on the channel
+    print(result.message)                                   # print the new msg
